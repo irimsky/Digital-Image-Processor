8a31c06aa82670825eed5571af969b53f3632729
irimsky <913031255@qq.com>
2020-10-17 18:54:24 +0800

    更新 错切

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace DIP
     public partial class MainWindow : Window
     {
         // 当前位图与其指针
-        Bitmap bmp;
+        Bitmap bmp = null;
         IntPtr bip;
         // 状态、 原图信息
         string status, info;
@@ -81,6 +81,26 @@ namespace DIP
                         MessageBox.Show(ex.Message, "错误");
                     }
                 }
+                else if(status=="放大")
+                {
+                    TextBox tbw = (TextBox)FindName("wscale"),
+                        tbh = (TextBox)FindName("hscale");
+                    try
+                    {
+                        double wscale = Convert.ToDouble(tbw.Text);
+                        double hscale = Convert.ToDouble(tbh.Text);
+                        if (wscale < 1 || hscale < 1)
+                        {
+                            MessageBox.Show("请输入有效范围的数字");
+                            return;
+                        }
+                        maximize(hscale, wscale);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "错误");
+                    }
+                }
             }
             else
             {
@@ -102,79 +122,131 @@ namespace DIP
                     tb.Text = info;
                     grid.Children.Add(tb);
                 }
-                else if(txt == "原图信息")
+                else
                 {
-                    grid.Children.Clear();
-                    TextBlock tb = new TextBlock();
-                    tb.Margin = new Thickness(10, 5, 10, 10);
-                    tb.FontSize = 16;
-                    tb.Text = info;
-                    grid.Children.Add(tb);
-                }
-                else if(txt =="旋转")
-                {
-                    grid.Children.Clear();
-                    Label lb = new Label();
-                    lb.Content = "角度:";
-                    lb.VerticalAlignment = VerticalAlignment.Center;
-                    grid.Children.Add(lb);
+                    if(bmp==null)
+                    {
+                        MessageBox.Show("请打开一张图片！");
+                        return;
+                    }
+                    if (txt == "原图信息")
+                    {
+                        grid.Children.Clear();
+                        TextBlock tb = new TextBlock();
+                        tb.Margin = new Thickness(10, 5, 10, 10);
+                        tb.FontSize = 16;
+                        tb.Text = info;
+                        grid.Children.Add(tb);
+                    }
+                    else if (txt == "旋转")
+                    {
+                        grid.Children.Clear();
+                        Label lb = new Label();
+                        lb.Content = "角度:";
+                        lb.VerticalAlignment = VerticalAlignment.Center;
+                        grid.Children.Add(lb);
 
-                    TextBox tb = new TextBox();
-                    tb.Width = 40;
-                    tb.Height = 20;
-                    tb.Margin = new Thickness(10,0,20,0);
-                    if (FindName("angle") != null)
-                        grid.UnregisterName("angle");
-                    grid.RegisterName("angle", tb);
-                    grid.Children.Add(tb);
+                        TextBox tb = new TextBox();
+                        tb.Width = 40;
+                        tb.Height = 20;
+                        tb.Margin = new Thickness(10, 0, 20, 0);
+                        if (FindName("angle") != null)
+                            grid.UnregisterName("angle");
+                        grid.RegisterName("angle", tb);
+                        grid.Children.Add(tb);
 
-                    Button btn = new Button();
-                    btn.Margin = new Thickness(20, 20, 20, 20);
-                    btn.Content = "确认";
-                    btn.Height = 20;
-                    btn.Width = 50;
-                    btn.Click += Button_Click;
-                    grid.Children.Add(btn);
-                }
-                else if(txt=="缩小")
-                {
-                    grid.Children.Clear();
-                    Label lb = new Label();
-                    lb.Content = "宽缩小幅度（0-1）:";
-                    lb.VerticalAlignment = VerticalAlignment.Center;
-                    grid.Children.Add(lb);
+                        Button btn = new Button();
+                        btn.Margin = new Thickness(20, 20, 20, 20);
+                        btn.Content = "确认";
+                        btn.Height = 20;
+                        btn.Width = 50;
+                        btn.Click += Button_Click;
+                        grid.Children.Add(btn);
+                    }
+                    else if (txt == "缩小")
+                    {
+                        grid.Children.Clear();
+                        Label lb = new Label();
+                        lb.Content = "宽缩小幅度（0-1）:";
+                        lb.VerticalAlignment = VerticalAlignment.Center;
+                        grid.Children.Add(lb);
 
-                    TextBox tbw = new TextBox();
-                    tbw.Width = 40;
-                    tbw.Height = 20;
-                    tbw.Margin = new Thickness(10, 0, 20, 0);
-                    if (FindName("wscale") != null)
-                        grid.UnregisterName("wscale");
-                    grid.RegisterName("wscale", tbw);
-                    grid.Children.Add(tbw);
+                        TextBox tbw = new TextBox();
+                        tbw.Width = 40;
+                        tbw.Height = 20;
+                        tbw.Margin = new Thickness(10, 0, 20, 0);
+                        if (FindName("wscale") != null)
+                            grid.UnregisterName("wscale");
+                        grid.RegisterName("wscale", tbw);
+                        grid.Children.Add(tbw);
 
-                    Label lb2 = new Label();
-                    lb2.Content = "高缩小幅度（0-1）:";
-                    lb2.VerticalAlignment = VerticalAlignment.Center;                    
-                    lb2.Margin = new Thickness(20, 0, 0, 0);
-                    grid.Children.Add(lb2);
+                        Label lb2 = new Label();
+                        lb2.Content = "高缩小幅度（0-1）:";
+                        lb2.VerticalAlignment = VerticalAlignment.Center;
+                        lb2.Margin = new Thickness(20, 0, 0, 0);
+                        grid.Children.Add(lb2);
 
-                    TextBox tbh = new TextBox();
-                    tbh.Width = 40;
-                    tbh.Height = 20;
-                    tbh.Margin = new Thickness(10, 0, 20, 0);
-                    if (FindName("hscale") != null)
-                        grid.UnregisterName("hscale");
-                    grid.RegisterName("hscale", tbh);
-                    grid.Children.Add(tbh);
+                        TextBox tbh = new TextBox();
+                        tbh.Width = 40;
+                        tbh.Height = 20;
+                        tbh.Margin = new Thickness(10, 0, 20, 0);
+                        if (FindName("hscale") != null)
+                            grid.UnregisterName("hscale");
+                        grid.RegisterName("hscale", tbh);
+                        grid.Children.Add(tbh);
 
-                    Button btn = new Button();
-                    btn.Margin = new Thickness(20, 20, 20, 20);
-                    btn.Content = "确认";
-                    btn.Height = 20;
-                    btn.Width = 50;
-                    btn.Click += Button_Click;
-                    grid.Children.Add(btn);
+                        Button btn = new Button();
+                        btn.Margin = new Thickness(20, 20, 20, 20);
+                        btn.Content = "确认";
+                        btn.Height = 20;
+                        btn.Width = 50;
+                        btn.Click += Button_Click;
+                        grid.Children.Add(btn);
+                    }
+                    else if (txt == "放大")
+                    {
+                        grid.Children.Clear();
+                        Label lb = new Label();
+                        lb.Content = "宽放大幅度（＞1）:";
+                        lb.VerticalAlignment = VerticalAlignment.Center;
+                        grid.Children.Add(lb);
+
+                        TextBox tbw = new TextBox();
+                        tbw.Width = 40;
+                        tbw.Height = 20;
+                        tbw.Margin = new Thickness(10, 0, 20, 0);
+                        if (FindName("wscale") != null)
+                            grid.UnregisterName("wscale");
+                        grid.RegisterName("wscale", tbw);
+                        grid.Children.Add(tbw);
+
+                        Label lb2 = new Label();
+                        lb2.Content = "高放大幅度（＞1）:";
+                        lb2.VerticalAlignment = VerticalAlignment.Center;
+                        lb2.Margin = new Thickness(20, 0, 0, 0);
+                        grid.Children.Add(lb2);
+
+                        TextBox tbh = new TextBox();
+                        tbh.Width = 40;
+                        tbh.Height = 20;
+                        tbh.Margin = new Thickness(10, 0, 20, 0);
+                        if (FindName("hscale") != null)
+                            grid.UnregisterName("hscale");
+                        grid.RegisterName("hscale", tbh);
+                        grid.Children.Add(tbh);
+
+                        Button btn = new Button();
+                        btn.Margin = new Thickness(20, 20, 20, 20);
+                        btn.Content = "确认";
+                        btn.Height = 20;
+                        btn.Width = 50;
+                        btn.Click += Button_Click;
+                        grid.Children.Add(btn);
+                    }
+                    else if (txt == "错切")
+                    {
+                        shear(0.5, 0);
+                    }
                 }
             }
             
--- operation.cs
+++ operation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -201,8 +202,8 @@ namespace DIP
         private void minimize(double k1, double k2)
         {
             double di = 1 / k1, dj = 1 / k2;
-            int nw = (int)Math.Round(bmp.Width * k2, 0), 
-                nh = (int)Math.Round(bmp.Height * k1, 0);
+            int nw = (int)Math.Round(bmp.Width * k2), 
+                nh = (int)Math.Round(bmp.Height * k1);
             Bitmap bmp_ = new Bitmap(nw, nh);
             for(int i=0;i<nh;i++)
             {
@@ -210,6 +211,8 @@ namespace DIP
                 {
                     int sx = (int)Math.Round(di * i), ex = (int)Math.Round(di * (i+1));
                     int sy = (int)Math.Round(dj * j), ey = (int)Math.Round(dj * (j+1));
+                    ey = Math.Min(bmp.Width, ey);
+                    ex = Math.Min(bmp.Height, ex);
                     int rsum = 0, gsum = 0, bsum = 0;
                     for (int ii = sx;ii<ex;ii++)
                     {
@@ -232,5 +235,155 @@ namespace DIP
             img.Source = bmp2img(ref bmp_);
         }
 
+        /// <summary>
+        /// 放大位图尺寸
+        /// </summary>
+        /// <param name="k1">高扩大幅度</param>
+        /// <param name="k2">宽扩大幅度</param>
+        private void maximize(double k1, double k2)
+        {
+            //double di = 1 / k1, dj = 1 / k2;
+            int nw = (int)Math.Round(bmp.Width * k2),
+                nh = (int)Math.Round(bmp.Height * k1);
+            Bitmap bmp_ = new Bitmap(nw, nh);
+
+            ArrayList widx = new ArrayList(), hidx = new ArrayList();
+
+            for(int i=0;i<bmp.Height;i++)
+            {
+                for(int j=0;j<bmp.Width;j++)
+                {
+                    int sx = (int)Math.Round(k1 * i), ex = (int)Math.Round(k1 * (i + 1)),
+                        sy = (int)Math.Round(k2 * j), ey = (int)Math.Round(k2 * (j + 1));
+                    if(i==bmp.Height-1 && j==bmp.Width-1)
+                    {
+                        bmp_.SetPixel(nw - 1, nh - 1, bmp.GetPixel(j, i));
+                        widx.Add(nw - 1);
+                        hidx.Add(nh - 1);
+                    }
+                    else if(i==bmp.Height-1)
+                    {
+                        bmp_.SetPixel(sy, nh - 1, bmp.GetPixel(j, i));
+                        widx.Add(sy);
+                    }
+                    else if(j==bmp.Width-1)
+                    {
+                        bmp_.SetPixel(nw - 1, sx, bmp.GetPixel(j, i));
+                        hidx.Add(sx);
+                    }
+                    else
+                    {
+                        bmp_.SetPixel(sy, sx, bmp.GetPixel(j, i));
+                        // widx.Add(sy);
+                    }                    
+                }
+            }
+            widx.Sort();
+            hidx.Sort();
+            // MessageBox.Show(widx.IndexOf(2).ToString());
+            for(int j = 0;j < nw;j++)
+            {
+                if (widx.IndexOf(j)>=0) 
+                {
+                    int prei = 0;
+                    for(int i=1;i<nh;i++)
+                    {
+                        if(hidx.IndexOf(i) == -1)
+                        {
+                            int posti = (int)hidx[hidx.IndexOf(prei) + 1];
+                            double d = (double)(i - prei) / (posti - prei);
+                            Color post = bmp_.GetPixel(j, posti);
+                            Color prev = bmp_.GetPixel(j, prei);
+
+                            Color tmp =
+                                Color.FromArgb(
+                                    (int)Math.Round(prev.R + (post.R - prev.R) * d),
+                                    (int)Math.Round(prev.G + (post.G - prev.G) * d),
+                                    (int)Math.Round(prev.B + (post.B - prev.B) * d)
+                                );
+                            bmp_.SetPixel(j, i, tmp);
+                        }
+                        else                        
+                            prei = i;
+                        
+                    }
+                }
+            }
+            for(int i = 0;i < nh;i++)
+            {
+                    int prej = 0;
+                    for (int j = 1; j < nw; j++)
+                    {
+                        if (widx.IndexOf(j) == -1)
+                        {
+                            int postj = (int)widx[widx.IndexOf(prej) + 1];
+                            double d = (double)(j - prej) / (postj - prej);
+                            Color post = bmp_.GetPixel(postj, i);
+                            Color prev = bmp_.GetPixel(prej, i);
+                            Color tmp =
+                                Color.FromArgb(
+                                    (int)Math.Round(prev.R + (post.R - prev.R) * d),
+                                    (int)Math.Round(prev.G + (post.G - prev.G) * d),
+                                    (int)Math.Round(prev.B + (post.B - prev.B) * d)
+                                );
+                            bmp_.SetPixel(j, i, tmp);
+                        }
+                    
+                        else
+                            prej = j;
+
+                    }
+                
+            }
+            img.Source = bmp2img(ref bmp_);
+        }
+
+
+        private void shear(double c, double b)
+        {
+
+            int[] range = { bmp.Height, 0, bmp.Width, 0 };
+            int nx = (int)Math.Round(bmp.Height + c * bmp.Width),
+                ny = (int)Math.Round(bmp.Width + b * bmp.Height);
+            range[0] = Math.Min(range[0], nx);
+            range[1] = Math.Max(range[1], nx);
+            range[2] = Math.Min(range[2], ny);
+            range[3] = Math.Max(range[3], ny);
+
+            nx = (int)Math.Round(c * bmp.Width);
+            ny = bmp.Width;
+            range[0] = Math.Min(range[0], nx);
+            range[1] = Math.Max(range[1], nx);
+            range[2] = Math.Min(range[2], ny);
+            range[3] = Math.Max(range[3], ny);
+
+            nx = bmp.Height;
+            ny = (int)Math.Round(b * bmp.Height);
+            range[0] = Math.Min(range[0], nx);
+            range[1] = Math.Max(range[1], nx);
+            range[2] = Math.Min(range[2], ny);
+            range[3] = Math.Max(range[3], ny);
+
+            range[0] = Math.Min(range[0], 0);
+            range[1] = Math.Max(range[1], 0);
+            range[2] = Math.Min(range[2], 0);
+            range[3] = Math.Max(range[3], 0);
+            int xoff = 0, yoff = 0;
+            if (range[0] < 0)
+                xoff = -range[0];
+            if (range[2] < 0)
+                yoff = -range[2];
+            Bitmap bmp_ = new Bitmap(range[3]-range[2]+1, range[1]-range[0]+1);
+            for(int i=0;i<bmp.Height;i++)
+            {
+                for(int j=0;j<bmp.Width;j++)
+                {
+                    int xx = (int)Math.Round(i + c * j) + xoff, 
+                        yy = (int)Math.Round(j + b * i) + yoff;
+                    bmp_.SetPixel(yy, xx, bmp.GetPixel(j, i));
+                }
+            }
+            img.Source = bmp2img(ref bmp_);
+        }
     }
 }
